6c4013d9bb4d138df08af34a1ad357d9e902e867
Patricia Ferrer <patricia.ferreira@esx.com.br>
2025-03-27 18:08:12 -0300

    app

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using NavalBattle.Domain.Config;
+using NavalBattle.Domain.Enums;
+using NavalBattle.Domain.Models;
+using NavalBattle.Domain.Services.Implementations;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.Development.json", optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var settings = configuration.Get<AppSettings>();
+
+var messageService = new MessageService(
+    settings.ServiceBus.ConnectionString,
+    settings.ServiceBus.TopicName,
+    settings.ServiceBus.SubscriptionName,
+    new CryptoService(settings.Ship.CryptoKey));
+
+var battleService = new BattleService(
+    messageService,
+    new CryptoService(settings.Ship.CryptoKey),
+    settings.Ship.Name);
+
+var coordinator = new BattleCoordinator(battleService, messageService, settings.Ship.Name);
+await coordinator.StartAsync();
+
+Console.WriteLine("Batalha iniciada! Pressione qualquer tecla para sair...");
+Console.ReadKey();
